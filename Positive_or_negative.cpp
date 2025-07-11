8414cdd9314a9907a0b83563a361ba316bb0da3d
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-07-11 20:26:44 +0530

    Add files via upload

---
+++ Positive_or_negative.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter number:"<<endl;
+    cin>>num;
+    if (num==0){
+        cout<< num<<" is zero"<<endl;
+    }
+    else if (num>0)
+    {
+       cout<<num<<" is a positive number\n";
+    }
+    else
+    {
+        cout<<num<<" is a negative number";
+    }
+}
