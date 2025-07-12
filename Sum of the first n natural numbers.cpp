2c79ba6ffb82f49cf7a4c7816ca066cf970ee481
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-07-12 17:13:36 +0530

    Add files via upload

---
+++ Sum of the first n natural numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum=0;
+    int num;
+    cout<<"Enter n value:"<<endl;
+    cin>>num;
+    for(int i=1;i<=num;i++)
+    {
+        sum=sum+i;
+    }
+    cout<<"Sum of the first "<<num<<" natural numbers is "<<sum ;
+}
