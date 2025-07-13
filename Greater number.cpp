2c682c6fe23ccd4da34e430e7ddcb873fa185e8e
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-07-13 19:28:38 +0530

    Add files via upload

---
+++ Greater number.cpp
@@ -0,0 +1,17 @@
+#inlcude<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter two numbers:";
+    cin>>num1;
+    cin>>num2;
+    if (num1>num2)
+    {
+        cout<<num1 <<" is greater ."<<endl;
+    }
+    else{
+        cout<<num2<<" is greater .";
+    }
+
+}
