28e38805dcb98d88eb51ac9c9b326e56755041d0
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-07-19 07:04:26 +0530

    Add files via upload

---
+++ Sum of two numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a,b;
+   float x,y;
+   cout<<"Enter integer values a and b:"<<endl;
+   cin>>a;
+   cin >>b;
+   cout<<"Enter float values x and y:"<<endl;
+   cin>>x;
+   cin >>y;
+   cout<<"The sum of two integer values "<<a<<" and "<<b<<" is "<<a+b<<endl;
+   cout<<"The sum of two float values "<<x<<" and "<<y<<" is "<<x+y;
+    return 0;
+}
