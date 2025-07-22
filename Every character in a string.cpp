238c285114c409d9afd1837afef643ae1afbbea3
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-07-22 16:55:53 +0530

    Add files via upload

---
+++ Every character in a string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char str[20];
+    cout<<"Enter String:"<<endl;
+    cin>>str;
+    for(int i=0;i<strlen(str);i++)
+    {
+        cout<<str[i]<<endl;
+    }
+
+}
