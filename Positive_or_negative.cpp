be67984252d42aa1e020723be07212c30a9e5223
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2026-01-06 16:33:54 +0530

    Fix prompt message for entering a number

--- Positive_or_negative.cpp
+++ Positive_or_negative.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int num;
-    cout<<"enter number:"<<endl;
+    cout<<"Enter number:"<<endl;
     cin>>num;
     if (num==0){
         cout<< num<<" is zero"<<endl;
